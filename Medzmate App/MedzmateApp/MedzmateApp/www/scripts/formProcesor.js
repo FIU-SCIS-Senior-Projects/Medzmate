b1588724f99ccc480ff49849db5f91833276cfaa
pedro <usa.pedro@gmail.com>
2015-09-17 23:39:06 -0400

    added a lot of CSS style, and replaced almost all controls with jquerryMobile controls

---
+++ Medzmate App/MedzmateApp/MedzmateApp/www/scripts/formProcesor.js
@@ -0,0 +1,16 @@
+﻿function navigateToLoadingDeck() {
+    window.location = "loadingDeckSelector.html";
+}
+function proccessForm(e) {
+    var medicine = document.getElementById("element_1").value;
+    var str = '';
+    var elem = document.getElementById('form_1050292').elements;
+    for (var i = 0; i < elem.length; i++) {
+        str += "\n" + elem[i].name + " ";
+        str += elem[i].value + " ";
+        str += "...";
+    }
+
+    alert("medicine " + medicine + " elemts " + str);
+
+}
