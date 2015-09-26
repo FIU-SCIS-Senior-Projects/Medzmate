461240e725a3b1d26b73019544bca8b07b2b8991
pedro <usa.pedro@gmail.com>
2015-09-26 12:49:45 -0400

    Use local storage to keep track of straws being used

--- Medzmate App/MedzmateApp/MedzmateApp/www/scripts/formProcesor.js
+++ Medzmate App/MedzmateApp/MedzmateApp/www/scripts/formProcesor.js
@@ -2,13 +2,19 @@
     window.location = "loadingDeckSelector.html";
 }
 
+function populate(frm, data) {
+    //populate data from saved file
+}
+
 function initialize() {
+    document.getElementById('Straw_id').value = window.localStorage.getItem("currentStraw");
+    
     document.getElementById('form_1050292').addEventListener('submit', function (event) {
         event.preventDefault();
         var jobj = $(this).serializeArray();
-      //  var test = " data: " + $(this).serialize();
-      //  alert(test);
-        alert(JSON.stringify(jobj, null, 4));
+        var test = JSON.stringify(jobj, null, 4);
+        
+        alert(test);
         navigateToLoadingDeck();
     });
 }
