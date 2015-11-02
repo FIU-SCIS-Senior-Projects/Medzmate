9674f7d3eb2431eefede23e670ebbcd8c3c6b1a2
jcorr091 <jcorr091@fiu.edu>
2015-11-02 02:13:09 -0500

    Test layout

--- Medzmate Station/LexerTests.cpp
+++ Medzmate Station/LexerTests.cpp
@@ -2,6 +2,17 @@
 #include <list>
 #include "Lexer.h"
 
+void TestGet(Lexer _lexer){
+    
+}
+
+void TestPeek(Lexer _lexer){
+    
+}
+
+void TestNext(Lexer _lexer){
+    
+}
 
 int main(int argc, char **argv) {
     
