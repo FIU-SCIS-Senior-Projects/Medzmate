91670e72d652dc8ef9a48988a8b497883e2d2774
pedro <usa.pedro@gmail.com>
2015-11-21 15:35:16 -0500

    Trying to add popup

--- Medzmate App/MedzmateApp/MedzmateApp/www/scripts/main.js
+++ Medzmate App/MedzmateApp/MedzmateApp/www/scripts/main.js
@@ -1,7 +1,10 @@
-﻿
-document.addEventListener("backbutton", onBackKeyDown, false);
-function onBackKeyDown(e) {
-    console.log("backbutton pressed")
-    e.preventDefault();
-    navigateToLogin();
-}
+﻿$(document).ready(function () {
+    document.addEventListener("backbutton", onBackKeyDown, false);
+    function onBackKeyDown(e) {
+        console.log("backbutton pressed")
+        e.preventDefault();
+        navigateToLogin();
+    }
+    $(".ui-corner-all").hide();
+    $(".ui-corner-all").toggle();
+});
