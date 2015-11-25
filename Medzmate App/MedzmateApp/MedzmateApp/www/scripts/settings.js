b07ab02ab7c85d6880a79b670330691671f76242
pedro <usa.pedro@gmail.com>
2015-11-24 22:58:28 -0500

    Re factoring data transmission

--- Medzmate App/MedzmateApp/MedzmateApp/www/scripts/settings.js
+++ Medzmate App/MedzmateApp/MedzmateApp/www/scripts/settings.js
@@ -2,36 +2,11 @@
     window.localStorage.setItem(ID, jsonData);
 }
 
-function sendToStation(ID, jsonData) {
-    var medzMateId = window.localStorage.getItem("currentStation");
-    var fName = ID + ".json"
-    // alert(jsonData);
-    $.ajax({
-        url: "http://" + medzMateId + ":3080/saveSettings",
-        // dataType: "jsonp"
-        data: { fileName: fName, patientData: jsonData },
-        // fileName: 'TestStraw123456',
-        type: 'POST',
-        jsonpCallback: 'callback', // this is not relevant to the POST anymore
-        success: function (data) {
-            console.log('Success: ', data);
-            if (data == "OK loged") {
-                isValid = 0;
-                console.log("isValid seccess", isValid);
-            }
-        },
-        error: function (xhr, status, error) {
-            console.log('Error: ' + error.message);
-        },
-        //async: false
-    });
-}
-
 function onSubmitClick() {
     var medzMateId = window.localStorage.getItem("currentStation");
     var patientInfoData = window.localStorage.getItem("temp");
     saveDataToFile(medzMateId + '-Settings', patientInfoData);
-    sendToStation(medzMateId + '-Settings', patientInfoData);
+    sendToStation(medzMateId + '-Settings', patientInfoData,"saveSettings");
     createLog(medzMateId + '-Settings', patientInfoData)
     console.log("submited popup");
 }
